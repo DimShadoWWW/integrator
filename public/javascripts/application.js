a5e8c5c330dba078a3c39ed296289bbf521f3112
Anielkis Herrera <anielkis@gmail.com>
2014-05-07 08:10:51 -0500

    changing the web interface

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,64 +1,66 @@
-$(document).ready(function(){
-    $("td.timeago").timeago();
-    var options = {series: {
-                    pie: {
+var iteration = 0;
+var options = {series: {
+                pie: {
+                    show: true,
+                    label: {
                         show: true,
-                        label: {
-                            show: true,
-                            radius: 3/4,
-                            formatter: function(label, series){
-                                return '<div style="font-size:8pt;text-align:center;padding:2px;color:rgba(111, 73, 73, 1);">'+label+'<br/>'+Math.round(series.percent)+'% ('+series.data[0][1]+')</div>';
-                            }
-                        },
-                        background: {
-                            opacity: 0.5
-                        },
-                        offset: {
-                            left: -25
+                        radius: 3/4,
+                        formatter: function(label, series){
+                            return '<div style="font-size:8pt;text-align:center;padding:2px;color:rgba(111, 73, 73, 1);">'+label+'<br/>'+Math.round(series.percent)+'% ('+series.data[0][1]+')</div>';
                         }
+                    },
+                    background: {
+                        opacity: 0.5
+                    },
+                    offset: {
+                        left: -25
                     }
                 }
-            };
-
-    // Initiate a recurring data update
-    var data_containers = [];
-    var data_images = [];
-
-    var iteration = 0;
-    function fetchData() {
-        ++iteration;
-        function onDataReceived(series) {
-            CreateButtons(series);
-
-            data_containers = [];
-            for (var i in series.Containers.Status) {
-                data_containers.push({
-                    label:   i,
-                    data: series.Containers.Status[i]
-                    });
             }
+        };
 
-            var data_images = [];
-            // var legend_images="<table class=\"table table-striped\"><thead><tr><th>Name</th><th>Value</th></tr></thead><tbody>";
-            for (var i in series.Images.Status) {
-                data_images.push({
-                    label:   i,
-                    data: series.Images.Status[i]
-                    });
-            }
-            $.plot("#placeholder-containers", data_containers, options);
-            $.plot("#placeholder-images", data_images, options);
+// Initiate a recurring data update
+var data_containers = [];
+var data_images = [];
+
+function fetchData() {
+    ++iteration;
+    function onDataReceived(series) {
+        CreateButtons(series);
+
+        data_containers = [];
+        for (var i in series.Containers.Status) {
+            data_containers.push({
+                label:   i,
+                data: series.Containers.Status[i]
+                });
         }
-        // Normally we call the same URL - a script connected to a
-        // database - but in this case we only have static example
-        // files, so we need to modify the URL.
-        $.ajax({
-            url: "/api/status",
-            type: "GET",
-            dataType: "json",
-            success: onDataReceived
-        });
+
+        var data_images = [];
+        // var legend_images="<table class=\"table table-striped\"><thead><tr><th>Name</th><th>Value</th></tr></thead><tbody>";
+        for (var i in series.Images.Status) {
+            data_images.push({
+                label:   i,
+                data: series.Images.Status[i]
+                });
+        }
+        $.plot("#placeholder-containers", data_containers, options);
+        $.plot("#placeholder-images", data_images, options);
     }
+    // Normally we call the same URL - a script connected to a
+    // database - but in this case we only have static example
+    // files, so we need to modify the URL.
+    $.ajax({
+        url: "/api/status",
+        type: "GET",
+        dataType: "json",
+        success: onDataReceived
+    });
+}
+
+fetchData();
+$(document).ready(function(){
+    $("td.timeago").timeago();
 
     fetchData();
     var timerID = setInterval(function(){fetchData()}, 3000);
