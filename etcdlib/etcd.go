c8a5f16c62e05bfa0cb41b754d64e3317df506de
Anielkis Herrera <anielkis@gmail.com>
2014-05-09 15:51:06 -0500

    restructuring the code

--- etcdlib/etcd.go
+++ etcdlib/etcd.go
@@ -1,162 +1,30 @@
 package etcdlib
 
 import (
-	"encoding/json"
 	"fmt"
 	"github.com/coreos/go-etcd/etcd"
 	"strings"
 )
 
 type EtcdClient struct {
-	client *etcd.Client
-}
-
-type DnsEntry struct {
-	Port     int
-	Priority int
-	Host     string
-}
-
-type DnsHost struct {
-	EtcdKey  string
-	Hostname string
-	Entry    DnsEntry
-}
-
-func (client *EtcdClient) GetDNSHostnames(path string) ([]DnsHost, error) {
-	var paths []DnsHost
-	recursive := true
-	sorted := true
-	r, err := client.client.Get(path+"/", sorted, recursive)
-	if err != nil {
-		return nil, err
-	}
-	for _, a := range r.Node.Nodes {
-		if a.Dir {
-			for _, b := range a.Nodes {
-				if b.Dir {
-					for _, c := range b.Nodes {
-						if c.Dir {
-							for _, d := range c.Nodes {
-								hostpath := strings.Split(strings.SplitAfterN(d.Key, "/", 3)[2], "/")
-								for i, j := 0, len(hostpath)-1; i < j; i, j = i+1, j-1 {
-									hostpath[i], hostpath[j] = hostpath[j], hostpath[i]
-								}
-								hostname := strings.Join(hostpath, ".")
-								var entry DnsEntry
-								err := json.Unmarshal([]byte(d.Value), &entry)
-								if err != nil {
-									fmt.Println("error:", err)
-									return nil, err
-								}
-								host := DnsHost{
-									Hostname: hostname,
-									EtcdKey:  d.Key,
-									Entry:    entry,
-								}
-								paths = append(paths, host)
-							}
-						}
-					}
-				}
-			}
-		}
-	}
-
-	return paths, nil
-}
-
-func (client *EtcdClient) DeleteHostnameDNS(hostname, ipaddress string, port int) error {
-
-	entry := DnsEntry{
-		Host: ipaddress,
-		Port: port,
-	}
-	hostpath := strings.Split(hostname, ".")
-	for i, j := 0, len(hostpath)-1; i < j; i, j = i+1, j-1 {
-		hostpath[i], hostpath[j] = hostpath[j], hostpath[i]
-	}
-	host := DnsHost{
-		Hostname: hostname,
-		EtcdKey:  "/skydns/" + strings.Join(hostpath, "/"),
-		Entry:    entry,
-	}
-
-	if _, err := client.client.Delete(host.EtcdKey, true); err != nil {
-		return formatErr(err)
-	}
-
-	hostpath = strings.Split(host.EtcdKey, "/")
-	for i := 0; i < len(hostpath); i++ {
-		local_path := "/" + strings.Join(hostpath[0:len(hostpath)-i], "/")
-		response, err := client.client.Get(local_path, false, false)
-		if notFound(err) {
-			fmt.Printf("Key error: %s", err)
-			// return nil
-		} else {
-			if isEmptyDir(response.Node) {
-				client.client.Delete(response.Node.Key, true)
-			} else {
-				return nil
-			}
-		}
-	}
-	return nil
-}
-
-func (client *EtcdClient) CleanDNS() error {
-	var paths []string
-	recursive := true
-	sorted := true
-	r, err := client.client.Get("/skydns", sorted, recursive)
-	if err != nil {
-		fmt.Println(err)
-		return err
-	}
-	for _, a := range r.Node.Nodes {
-		if a.Dir {
-			for _, b := range a.Nodes {
-				if b.Dir {
-					for _, c := range b.Nodes {
-						if c.Dir {
-							paths = append([]string{c.Key}, paths...)
-						}
-					}
-					paths = append(paths, b.Key)
-				}
-			}
-			paths = append(paths, a.Key)
-		}
-	}
-	for _, path := range paths {
-		response, err := client.client.Get(path, false, false)
-		if notFound(err) {
-			fmt.Printf("Key error: %s", err)
-		}
-		if isEmptyDir(response.Node) {
-			client.client.Delete(response.Node.Key, true)
-		} else {
-			return nil
-		}
-	}
-	return nil
+	Client *etcd.Client
 }
 
 func NewEtcdClient(nodes []string) (*EtcdClient, error) {
 	client := etcd.NewClient(nodes)
 
 	b := &EtcdClient{
-		client: client,
+		Client: client,
 	}
 	return b, nil
 }
 
 func (s *EtcdClient) getVal(keys ...string) (string, bool) {
-	response, err := s.client.Get(strings.Join(keys, "/"), false, false)
-	if notFound(err) {
+	response, err := s.Client.Get(strings.Join(keys, "/"), false, false)
+	if NotFound(err) {
 		return "", false
 	}
-	if isDir(response.Node) {
+	if IsDir(response.Node) {
 		return "", false
 	}
 	return response.Node.Value, true
@@ -164,17 +32,17 @@ func (s *EtcdClient) getVal(keys ...string) (string, bool) {
 
 func (s *EtcdClient) getDirs(keys ...string) []string {
 	var out []string
-	response, err := s.client.Get(strings.Join(keys, "/"), true, true)
-	if notFound(err) {
+	response, err := s.Client.Get(strings.Join(keys, "/"), true, true)
+	if NotFound(err) {
 		return out
 	}
 
-	if response == nil || !isDir(response.Node) {
+	if response == nil || !IsDir(response.Node) {
 		return out
 	}
 
 	for _, srvNode := range response.Node.Nodes {
-		if isDir(srvNode) {
+		if IsDir(srvNode) {
 			out = append(out, srvNode.Key)
 		}
 	}
@@ -183,17 +51,17 @@ func (s *EtcdClient) getDirs(keys ...string) []string {
 
 func (s *EtcdClient) getVals(keys ...string) []Pair {
 	var out []Pair
-	response, err := s.client.Get(strings.Join(keys, "/"), true, true)
-	if notFound(err) {
+	response, err := s.Client.Get(strings.Join(keys, "/"), true, true)
+	if NotFound(err) {
 		return out
 	}
 
-	if !isDir(response.Node) {
+	if !IsDir(response.Node) {
 		return out
 	}
 
 	for _, srvNode := range response.Node.Nodes {
-		if !isDir(srvNode) {
+		if !IsDir(srvNode) {
 			out = append(out, Pair{srvNode.Key, srvNode.Value})
 		}
 	}
@@ -214,7 +82,7 @@ func join(keys ...string) string {
 	return strings.Join(keys, "/")
 }
 
-func formatErr(e error) error {
+func FormatErr(e error) error {
 	switch err := e.(type) {
 	case *etcd.EtcdError:
 		return fmt.Errorf("Key error: %s", err.Message)
@@ -222,7 +90,7 @@ func formatErr(e error) error {
 	return e
 }
 
-func notFound(err error) bool {
+func NotFound(err error) bool {
 	if err == nil {
 		return false
 	}
@@ -238,12 +106,12 @@ func isDupe(err error) bool {
 	return ok && eErr.ErrorCode == 105
 }
 
-func isDir(n *etcd.Node) bool {
+func IsDir(n *etcd.Node) bool {
 	return n != nil && n.Dir == true
 }
 
-func isEmptyDir(n *etcd.Node) bool {
-	if isDir(n) && len(n.Nodes) == 0 {
+func IsEmptyDir(n *etcd.Node) bool {
+	if IsDir(n) && len(n.Nodes) == 0 {
 		return true
 	}
 	return false
