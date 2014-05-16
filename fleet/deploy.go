9753fbeb122652581104fd82197b32a0b9cbcd73
Anielkis Herrera <anielkis@gmail.com>
2014-05-16 07:32:28 -0500

    Adding basic fleet deploy interface

---
+++ fleet/deploy.go
@@ -0,0 +1,43 @@
+package fleet
+
+import (
+	"bytes"
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+)
+
+func Deploy(fname string, address string) error {
+	_, err := os.Stat(fname)
+	if err != nil {
+		return err
+	}
+
+	fName := filepath.Base(fname)
+
+	fmt.Println("/usr/bin/fleetctl", "start", fName)
+	cmd := exec.Command("/usr/bin/fleetctl", "start", fName)
+
+	if address != "" {
+		fmt.Println("/usr/bin/fleetctl", "-endpoint=\""+address+"\"", "start", fName)
+		cmd = exec.Command("/usr/bin/fleetctl", "-endpoint=\""+address+"\"", "start", fName)
+	}
+
+	var stderr bytes.Buffer
+	cmd.Stderr = &stderr
+
+	var stdout bytes.Buffer
+	cmd.Stdout = &stdout
+
+	err = cmd.Run()
+	fmt.Println("error run:", err)
+	if err != nil {
+		fmt.Errorf("error: %s\n", err)
+		fmt.Printf("stderr: %s\n", stderr.String())
+		fmt.Printf("stdout: %s\n", stdout.String())
+		return err
+	}
+
+	return nil
+}
