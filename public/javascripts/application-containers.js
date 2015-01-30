73b019f3b8376a0e8f79cc7c89134fdbae08f5b7
Anielkis Herrera <anielkis@gmail.com>
2015-01-30 15:17:53 -0500

    Changing Id by ID in js

--- public/javascripts/application-containers.js
+++ public/javascripts/application-containers.js
@@ -3,12 +3,12 @@ function CreateButtons(series){
     for (var i in series.Containers.Containers) {
         buttons="";
         if (series.Containers.Containers[i].Status.split(" ")[0] == "Up") {
-            buttons = "<button type=\"button\" onclick=\"$.ajax({url: '/api/containers/stop/"+series.Containers.Containers[i].Id+"',type: 'GET',dataType: 'json'})\" data-loading-text=\"Stopping...\" class=\"btn btn-primary btn-stop-cont\">Stop</button>"
+            buttons = "<button type=\"button\" onclick=\"$.ajax({url: '/api/containers/stop/"+series.Containers.Containers[i].ID+"',type: 'GET',dataType: 'json'})\" data-loading-text=\"Stopping...\" class=\"btn btn-primary btn-stop-cont\">Stop</button>"
         } else {
-            buttons = "<button type=\"button\" onclick=\"$.ajax({url: '/api/containers/start/"+series.Containers.Containers[i].Id+"',type: 'GET',dataType: 'json'})\" data-loading-text=\"Starting...\" class=\"btn btn-primary btn-start-cont\">Start</button>"
-            buttons += "<button type=\"button\" onclick=\"$.ajax({url: '/api/containers/del/"+series.Containers.Containers[i].Id+"',type: 'GET',dataType: 'json'})\" data-loading-text=\"Removing...\" class=\"btn btn-primary btn-del-cont\">Delete</button>"
+            buttons = "<button type=\"button\" onclick=\"$.ajax({url: '/api/containers/start/"+series.Containers.Containers[i].ID+"',type: 'GET',dataType: 'json'})\" data-loading-text=\"Starting...\" class=\"btn btn-primary btn-start-cont\">Start</button>"
+            buttons += "<button type=\"button\" onclick=\"$.ajax({url: '/api/containers/del/"+series.Containers.Containers[i].ID+"',type: 'GET',dataType: 'json'})\" data-loading-text=\"Removing...\" class=\"btn btn-primary btn-del-cont\">Delete</button>"
         }
-        output+="<tr><td>"+series.Containers.Containers[i].Id.substring(0,12) +"</td><td>" + series.Containers.Containers[i].Names +"</td><td>" + series.Containers.Containers[i].Image +"</td><td>" + series.Containers.Containers[i].Status +
+        output+="<tr><td>"+series.Containers.Containers[i].ID.substring(0,12) +"</td><td>" + series.Containers.Containers[i].Names +"</td><td>" + series.Containers.Containers[i].Image +"</td><td>" + series.Containers.Containers[i].Status +
         "</td><td>"+buttons+"</td></tr>";
     }
     output+="</tbody></table>";
