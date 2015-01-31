9abf3b9a8a113e44bf4aef8262ef5b74b5b12b0c
Anielkis Herrera <anielkis@gmail.com>
2015-01-30 22:36:38 -0500

    activating image deletion

--- public/javascripts/application-images.js
+++ public/javascripts/application-images.js
@@ -1,7 +1,7 @@
 function CreateButtons(series){
     var output="<table class=\"table table-striped\"><thead><tr><th>ID</th><th>RepoTags</th><th>Created</th><th>Actions</th></tr></thead><tbody>";
     for (var i in series.Images.Images) {
-        buttons = "<button type=\"button\" href=\"/buildimage.html\" class=\"btn btn-primary btn-stop-cont\">Remove</button>"
+        buttons = "<button type=\"button\" href=\"/images/del/"+series.Images.Images[i].Id +"\" class=\"btn btn-primary btn-stop-cont\">Remove</button>"
         output+="<tr><td>"+series.Images.Images[i].Id.substring(0,12) +"</td><td>" + series.Images.Images[i].RepoTags[0] +"</td><td class=\"timeago\">" + series.Images.Images[i].Created +"</td><td>"+buttons+"</td></tr>";
     }
     output+="</tbody></table>";
